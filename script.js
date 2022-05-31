2d3f95ca5f2e0a0cee4b13069068c2c55924df58
John <john.houston0411@gmail.com>
2022-05-31 09:36:18 -0500

    script.js

--- script.js
+++ script.js
@@ -1,29 +1,63 @@
 const add = document.querySelector(".add-book");
+const body = document.querySelector("body");
+let fname;
+let lname;
+let year;
+let submit;
+let exit;
+let form;
+
+
+let books = [];
+
+const bookInfo = function(title, author, year){
+  this.title = title;
+  this.author = author;
+  this.year = year;
+}
+
+// body.addEventListener("click", (event)=>{
+//
+//   if(event.target.value === "Submit"){
+//
+//   }
+//   else if(event.target.value === "Exit"){
+//     console.log("exit")
+//   }
+//
+//   event.stopPropagation();
+// })
 add.addEventListener("click",(event)=>{
-  console.log(event)
   add.classList.add("hide-add-button")
   createform();
+  event.stopPropagation();
 })
+
 function createform(){
-  const form = document.createElement("form");
+  form = document.createElement("form");
   form.classList.add("book-card-form");
 
   const fname = document.createElement("input");
   fname.setAttribute("type", "text")
   fname.setAttribute("name", "title")
   fname.setAttribute("placeholder", "Book Title")
+  fname.classList.add("form-input");
+  fname.setAttribute("required"," ");
 
   const lname = document.createElement("input");
   lname.setAttribute("type", "text")
   lname.setAttribute("name", "Author")
   lname.setAttribute("placeholder", "Book Author")
+  lname.classList.add("form-input");
+  lname.setAttribute("required"," ");
 
   const year = document.createElement("input");
   year.setAttribute("type", "number")
   year.setAttribute("name", "Year")
   year.setAttribute("placeholder", "year Published")
   year.setAttribute('style','display:block');
-  console.log(year)
+  year.classList.add("form-input");
+  year.setAttribute("required"," ");
 
 
   const submit = document.createElement("input");
@@ -42,6 +76,23 @@ function createform(){
   form.appendChild(submit)
   form.appendChild(exit)
 
-  document.querySelector("body").append(form);
+  body.append(form);
 
+  form.addEventListener("click", (event)=>{
+
+    if(event.target.value === "Submit"){
+        // event.preventDefault()
+        // console.log(fname.value, lname.value, year.value)
+        // let item = new bookInfo(fname.value, lname.value, year.value)
+        books.push(new bookInfo(fname.textContent, lname.textContent, year.textContent));
+          console.log(books[0])
+
+
+
+      }
+      else if(event.target.value === "Exit"){
+        console.log("exit")
+      }
+  })
 }
+console.log(books[0])
